2ffb9f0c2053c570cff74a711ab75f8bd52ca656
maddiemcroy <mmcroy@gatech.edu>
2020-10-30 20:25:01 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# webdev-basics-tutorial
+
+Source code for workshop run for Computational Media Ambassadors in fall 2020, teaching webdev basics by coding a basic resume/portfolio website.
---
+++ index.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+    $('#header').animate({opacity: 1}, 1000);
+    $('#about-me').delay(1000).animate({opacity: 1}, 1000);
+    $('#education').delay(2000).animate({opacity: 1}, 1000);
+    $('#experience').delay(3000).animate({opacity: 1}, 1000);
+    $('#skills').delay(4000).animate({opacity: 1}, 1000);
+    $('#projects').delay(5000).animate({opacity: 1}, 1000);
+};
